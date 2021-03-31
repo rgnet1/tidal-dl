8444e88ada95ed4a4da5385a7d1ac4944d5863ce
Ramzey <rgnet1@gmail.com>
2021-03-31 14:42:45 -0700

    added output for login script

--- tidal-login.sh
+++ tidal-login.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 tidal-dl
+echo "Moving login token to proper directory"
 cp ~/.tidal-dl.token.json /production/www/cgi-bin/.tidal-dl.token.json
+echo "Changing file permission for tidal-dl access"
 chmod 666 /production/www/cgi-bin/.tidal-dl.token.json
